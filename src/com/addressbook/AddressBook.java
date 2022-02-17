f5b8a1a94d06f4c89c6d6e9825ac591d24d87fc9
divyanaC999 <divyachourasiya999@gmail.com>
2022-02-17 23:22:28 +0530

    commit

---
+++ src/com/addressbook/AddressBook.java
@@ -0,0 +1,10 @@
+package com.addressbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Address Book System ");
+	}
+
+}
